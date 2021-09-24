6d865fa68ee59efad959f2132fe1f50261e86815
HabOnGit <30337222+habibmohammed@users.noreply.github.com>
2021-09-23 23:18:41 -0400

    update index.php

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 
       //connection credientials
       $conn = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
+      //$conn = new mysqli($servername, $username, $password, $database);
 
       // Check connection
       if ($conn->connect_error) {
         die("Connection failed: " . mysqli_connect_error());
-        echo "died";
       }
       //process request
       if($_SERVER['REQUEST_METHOD'] == "POST"){
