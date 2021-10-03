93f01758af9b301cf723232cd577ef5d156b7cba
HabOnGit <30337222+habibmohammed@users.noreply.github.com>
2021-10-03 18:09:19 -0400

    Update time.php

--- scripts/time.php
+++ scripts/time.php
@@ -6,8 +6,7 @@
   $message = "";
   //connection credientials
 
-  //$conn = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
-    $conn = new mysqli("aa16s063ub6knw5.cszziiop74rm.us-east-1.rds.amazonaws.com","TeamYellow","Heywood2021","Capstone",3306);
+  $conn = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
   // Check connection
   if ($conn->connect_error) {
     die("Connection failed: " . mysqli_connect_error());
