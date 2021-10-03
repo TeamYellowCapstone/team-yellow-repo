98789e92c55e38abe58459c7830886e0b52dfe98
HabOnGit <30337222+habibmohammed@users.noreply.github.com>
2021-10-03 18:06:18 -0400

    Implemented AJAX to pull current time from the users browser instead of using the server time
    The body of the  view and the script are now separate.  And the script can be found in the scripts folder

--- retrieve-time.php
+++ retrieve-time.php
@@ -1,67 +1,9 @@
-<?php
-if(!isset($_COOKIE["id"])){
-  setcookie("id", 0);
-}
-  
-?>
 <!DOCTYPE html>
 <html>
   <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Hello World</title>
-  <?php
-    $message = "";
-    //connection credientials
-
-    $conn = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
-
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . mysqli_connect_error());
-
-    }
-    //process request
-    if($_SERVER['REQUEST_METHOD'] == "POST"){
-      if(isset($_POST["log"])){
-
-        // Insert Time and date to mysql
-        $time = new DateTime;
-        echo var_dump($time);
-        $sql = "INSERT INTO `LogTime`.`ButtonTime`
-                (`idButtonTime`)
-                VALUES
-                (current_time());";
-
-        if ($conn->query($sql) === True){
-          $last = $conn->insert_id;
-          setcookie("id",$last);
-          $message = "Current time has been logged successfully!";
-
-        }
-        else{
-          $message = "An error has occured. Please try again.";
-        }
-
-      }
-      if(isset($_POST["retrieve"])){
-        $userID = $_COOKIE['id'];
-        $sql = "SELECT * FROM `LogTime`.`ButtonTime` WHERE TimeID = $userID LIMIT 1";
-        $result = $conn->query($sql);
-        $time = 0;
-        if ($result->num_rows > 0){
-          while ($row = $result->fetch_assoc()){
-            $time = $row["idButtonTime"];
-          }
-          $message = "Last Log was at: ". $time;
-        }
-        else{
-          $message = "No log found!";
-        }       
-        
-      }
-    }
-    $conn->close();
-  ?>
+  
   <style>
     body{
       background-color: #6bb0b5;
@@ -92,17 +34,50 @@ if(!isset($_COOKIE["id"])){
   </head>
   <body >
     <div class="center">
-      <h1 class="center-text">Hello World</h1>
-      <p class="center-text">Please press the "Log Time" button to log your current time.</p>
-      <form method="POST">
-        <div class="center-div">
-          <input type="submit" id="log" name="log" value="Log Time">
-          <input type="submit" id="retrieve" name="retrieve" value="Retrieve Time">
-        </div>
-      </form>
-      <?php
-        echo "<p class='center-text'> $message </p>";
-      ?>
+    <h1 class="center-text">Hello World</h1>
+    <p class="center-text">Please press the "Log Time" button to log your current time.</p>
+    <div class="center-div">
+        <button id="log" onclick="log()">Log Time</button>
+        <button id="retrieve" onclick="retrieve()">Retrieve Time</button>
     </div>
+        <p class='center-text' id="status"></p>
+    </div>
+
+    <script>
+        function retrieve(){
+            ajax_request("scripts/time.php?v=retrieve"); //value = retrieve tells the server we are trying to retrieve
+        }
+        function log( ){
+            //create date object
+            var localeTime = new Date();
+
+            //convert date object into usable format
+            var year = localeTime.getFullYear(); //YYYY
+            var month = ("0" + (localeTime.getMonth() + 1)).slice(-2); //MM
+            var day = ("0" + localeTime.getDate()).slice(-2); //DD
+            var hour = localeTime.getHours(); // 0-23
+            hour = hour % 12 //change time format to 0-11
+            hour = (hour == 0) ? 12 : hour; // if hour is 0 set it to 12
+            hour = ("0" + hour).slice(-2); //hh
+            var minute = ("0" + localeTime.getMinutes()).slice(-2); //mm
+            var second = ("0" + localeTime.getSeconds()).slice(-2);//ss
+            var formattedTime = "" + year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second; //YYYY-MM-DD hh:mm:ss
+            
+            ajax_request("scripts/time.php?v="+formattedTime); //send the local time to the server
+        }
+        function ajax_request(url){
+            var requestObject = new XMLHttpRequest(); //create XMLHttpRequest object
+            
+            //if there is state change in our request and the response is ready and ok display the message
+            requestObject.onreadystatechange = function() {
+                if(this.readyState == 4 && this.status == 200){
+                    document.getElementById("status").innerHTML = requestObject.responseText;
+                }
+            };
+            requestObject.open("GET", url); //open the request channel or connection
+            requestObject.send(); //send the request
+        }
+
+    </script>
   </body>
 </html>
