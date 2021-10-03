5223066a70db0e42ba4340214fce5df0c846d7a7
rneang <90795300+rneang@users.noreply.github.com>
2021-10-03 19:08:43 -0400

    New Pull?

--- menu.php
+++ menu.php
@@ -5,15 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style.css"	type="text/css"	rel="stylesheet">
-    <title>Home Page</title>
+    <title>Menu</title>
 </head>
 
     <body>
-        
+
         <?php
             require "templates/navigation.php";
         ?>
-
+        
     </body>
 
 </html>
+
