fe43208512d40d2dd14c2e2dd1e842acf582e40e
Orsolya Papp <opapp@student.cscc.edu>
2021-10-06 17:19:24 -0400

    I added another style file to start the process of creating another design. This is only temporary.

--- templates/head.php
+++ templates/head.php
@@ -12,3 +12,6 @@
 ?>
 <link href="styles/style.css" type="text/css" rel="stylesheet">
 <link href="templates/templates-style.css" type="text/css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
