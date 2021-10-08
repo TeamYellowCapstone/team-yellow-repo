77b92273aa78db9505d7363bf8017fdcab2110d8
Orsolya Papp <opapp@student.cscc.edu>
2021-10-07 20:22:10 -0400

    Added font to all pages.

--- menu.php
+++ menu.php
@@ -1,6 +1,9 @@
 <!Doctype html>
 <html>
     <head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
     <?php
         require "templates/head.php";
     ?> 
